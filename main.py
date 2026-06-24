33d0e04f3b16daa12ad35683a633bd4f0c6dcb4a
Humoyun <mafiauz0224z@gmail.com>
2026-06-24 09:31:16 +0500

    Web server qoshildi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
+import os
 
+from aiohttp import web
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -10,6 +12,20 @@ from config import BOT_TOKEN
 import database as db
 from middlewares import ThrottlingMiddleware
 from handlers import user, admin
+
+
+async def handle_ping(request):
+    return web.Response(text="Bot ishlab turibdi ✅")
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle_ping)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.getenv("PORT", 10000))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
 
 
 async def main():
@@ -26,6 +42,8 @@ async def main():
     dp.include_router(user.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
+
+    await start_web_server()
     print("Bot ishga tushdi...")
     await dp.start_polling(bot)
 
